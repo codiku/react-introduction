a8c451e7c7e5120ea5deafb52815810706b07ae9
rlebhar <robin.lebhar.chill.studio@gmail.com>
2022-09-20 11:51:45 +0200

    wip

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+export default function App() {
+    return <h1>This is the root component of our app :) </h1>
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootDiv = document.getElementById('root')
+const reactRoot = ReactDOM.createRoot(rootDiv);
+reactRoot.render(
+   <App/>
+);
