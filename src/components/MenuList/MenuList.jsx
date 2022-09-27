32fa827f70d964f8017818baff8999a8527c0d76
rlebhar <robin.lebhar.chill.studio@gmail.com>
2022-09-27 13:34:07 +0200

    wip

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,28 +1,16 @@
 import { MenuListItem } from "../MenuListItem/MenuListItem";
 import s from "./style.module.css";
+import { DIFFICULTIES } from "./constant";
 export function MenuList(props) {
   return (
     <div className={s.container}>
-      <MenuListItem
-        isSelected={props.difficulty === "Low"}
-        onClick={props.onItemClick}
-        difficulty="Low"
-      />
-      <MenuListItem
-        isSelected={props.difficulty === "Medium"}
-        onClick={props.onItemClick}
-        difficulty="Medium"
-      />
-      <MenuListItem
-        isSelected={props.difficulty === "High"}
-        onClick={props.onItemClick}
-        difficulty="High"
-      />
-      <MenuListItem
-        isSelected={props.difficulty === "Insane"}
-        onClick={props.onItemClick}
-        difficulty="Insane"
-      />
+      {DIFFICULTIES.map((difficulty) => (
+        <MenuListItem
+          isSelected={props.difficulty === difficulty}
+          onClick={props.onItemClick}
+          difficulty={difficulty}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/components/MenuList/constant.js
@@ -0,0 +1,1 @@
+export const DIFFICULTIES = ["Low", "Medium", "High", "Insane"];
