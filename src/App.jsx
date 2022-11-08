5a51952d28037c6a9f4a953c51fcf096e9997167
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-08 10:34:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Greetings } from "./Greetings"
 export function App() {
-  function doThing() {
-    alert("Hello");
-  }
   return (
     <div>
-      <input type="checkbox" checked />
+      <input type="checkbox" checked onChange={function(){ console.log("You clicked")}} />
+      <h1>Welcome to the React world</h1>
+      <Greetings firstName="Rachel" lastName={"Green"} age={101} car={{brand:"Viper", color:"red", speed:200}} doSomething={function(){
+        console.log("GLAHEA GLAEAZ")
+      }} >
+        <img src="https://picsum/200/300" />
+      </Greetings>
     </div>
   );
 }
