01d5d91372ce30aaaca8c888bb8c3b9714a9bcea
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2022-11-08 10:35:36 +0100

    Update Greetings.jsx

--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -1,9 +1,10 @@
 export function Greetings(props) {
   console.log(props);
-  return (
-    <div>
-      Hi {props.firstName} {props.lastName} you are {props.age + 10}
-      {props.children}
-    </div>
-  );
+  
+  if(props.age > 100){
+    return <div> You are old </div>
+  }else{
+    return <div> You are young </div>
+  }
+  
 }
